e7b00adb82f1468fab46bae4936ba3b7fe5d8670
Gabriel Braz <gabrielbrazdacostag@gmail.com>
2024-10-21 20:17:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("contact_form").addEventListener(
+    "submit", function(event) {
+        event.preventDefault();
+        console.log(event);
+
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('message').value;
+
+        const mailtolink = `mailto:${email}?subject=${encodeURIComponent('Assunto Reunião')}
+        &body=${encodeURIComponent(message)}`;
+
+        window.location.href = mailtolink;
+    }
+);
